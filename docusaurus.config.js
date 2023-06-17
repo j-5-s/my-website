4259d6f4bc03f4ff6ec90ec59d02032579685528
James Charlesworth <james.charlesworth@gmail.com>
2023-06-17 12:21:10 -0400

    updates social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'img/j5s-social-card.jpg',
       navbar: {
         title: 'j5s',
         logo: {
